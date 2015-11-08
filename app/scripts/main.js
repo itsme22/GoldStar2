b2daf4c8ea97f4975c62ec6ccd590a838f58fcab
magdalenemc <magdalener.mcarthur@gmail.com>
2015-11-07 20:26:05 -0500

    2nd commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,45 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
-var Firebase = require("firebase");
-var GoldStar = new Firebase('https://goldiestar.firebaseio.com/');
+// require.config({
+//    baseUrl: 'scripts',
+//    paths: {
+//        'jquery': '../bower_components/jquery/jquery',
+//    },
+//    shim: {
+//        'jquery': {
+//            deps: [],
+//            exports: '$'
+//        }
+//    }
+// });
 
+//require(['jquery'], function($){
+   // now you can do what you always done with $
+   // $('body').remove();
+   // define(function(require) {
+	console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+	//var Firebase = require("firebase");
+	var GoldStar = new Firebase('https://goldiestar.firebaseio.com/');
+   // now you have the Zombie variable available in this context
+   // var zombie = new Zombie();
+   GoldStar.set({
+	name: "Sue"
+});
+console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+ // });
+//});
+
+$( ".newTeacher" ).click(function() {
+ alert( "Handler for .click() called." );
+});
+// var newUser = function(){
+// 	create 
+// };
+GoldStar.createUser({
+  email    : "magdalener.mcarthur@gmail.com",
+  password : "rose10"
+}, function(error, userData) {
+  if (error) {
+    console.log("Error creating user:", error);
+  } else {
+    console.log("Successfully created user account with uid:", userData.uid);
+  }
+});
