5a1a267edf85c66b22091d89741d5d7bef1dd5a5
magdalenemc <magdalener.mcarthur@gmail.com>
2015-11-08 09:39:18 -0500

    added a student an authenticated user

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,38 +1,17 @@
-// require.config({
-//    baseUrl: 'scripts',
-//    paths: {
-//        'jquery': '../bower_components/jquery/jquery',
-//    },
-//    shim: {
-//        'jquery': {
-//            deps: [],
-//            exports: '$'
-//        }
-//    }
-// });
 
-//require(['jquery'], function($){
-   // now you can do what you always done with $
-   // $('body').remove();
-   // define(function(require) {
-	console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
-	//var Firebase = require("firebase");
-	var GoldStar = new Firebase('https://goldiestar.firebaseio.com/');
-   // now you have the Zombie variable available in this context
-   // var zombie = new Zombie();
-   GoldStar.set({
+console.log('\'Allo \'Allo!'); 
+	
+var GoldStar = new Firebase('https://goldiestar.firebaseio.com/');
+   
+GoldStar.set({
 	name: "Sue"
 });
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
- // });
-//});
 
+console.log('\'Allo \'Allo!'); 
 $( ".newTeacher" ).click(function() {
  alert( "Handler for .click() called." );
 });
-// var newUser = function(){
-// 	create 
-// };
+
 GoldStar.createUser({
   email    : "magdalener.mcarthur@gmail.com",
   password : "rose10"
